5ee9d882873c5f5b8da0df359d0007497acb3bae
fnsrc <66776447+fnsrc@users.noreply.github.com>
2026-06-26 15:37:09 +0330

    script.js

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
-console.log("سایت با موفقیت بارگذاری شد.");
+// متن مورد نظر شما
+const designText = "طراحی سایت و وب سایت بصورت اختصاصی و کدنویسی در ایران سایت. طراحی سایت شما در دنیا به دو صورت قابل انجام است. طراحی سایت با قالب های آماده و طراحی سایت بصورت کدنویسی و اختصاصی. طراحی سایت در ایران سایت با کدنویسی اختصاصی و بصورت کدنویسی به زبان های PHP و NET. انجام می پذیرد. در این حالت وب سایت شما دارای قابلیت مدیریت تمامی قسمت ها توسط شما و بدون نیاز به طراح سایت می باشد.";
+
+// گرفتن المان‌ها
+const modal = document.getElementById("myModal");
+const btn = document.getElementById("web-design-card");
+const span = document.getElementsByClassName("close")[0];
+const modalText = document.getElementById("modal-text");
+
+// وقتی روی کارت کلیک شد
+btn.onclick = function() {
+  modal.style.display = "block";
+  modalText.innerText = designText;
+}
+
+// وقتی روی ضربدر کلیک شد
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// وقتی بیرون از پاپ‌آپ کلیک شد، بسته شود
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
